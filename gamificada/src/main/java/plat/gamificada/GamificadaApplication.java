73400c3fb43cb99bc1be2c073acebd8bba7abaca
GUSTAVO FERREIRA NUNES <gustavo.nunes21@senaisp.edu.br>
2026-03-23 21:55:39 -0300

    initial project

---
+++ gamificada/src/main/java/plat/gamificada/GamificadaApplication.java
@@ -0,0 +1,13 @@
+package plat.gamificada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamificadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamificadaApplication.class, args);
+	}
+
+}
